c9b4b763a6e45f7035b821da6cfb0735a79c2ce1
atsullivan <atsullivan23@gmail.com>
2015-03-19 18:54:28 -0700

    added appear.js for charts

--- profile-page-1/js/chart.js
+++ profile-page-1/js/chart.js
@@ -1,3 +1,26 @@
+(function (H) {
+    function deferRender (proceed) {
+        var series = this, 
+            $renderTo = $(this.chart.container.parentNode);
+     
+        // It is appeared, render it
+        if ($renderTo.is(':appeared')) {
+            proceed.call(series);
+            
+        // It is not appeared, halt renering until appear
+        } else  {
+            $renderTo.appear(); // Initialize appear plugin
+            $renderTo.on('appear', function () {
+                proceed.call(series);
+            });
+        }
+    };
+    
+    H.wrap(H.Series.prototype, 'render', deferRender);
+    
+}(Highcharts));
+
+
 $(function () {
     Highcharts.setOptions(Highcharts.theme);
     $('#container').highcharts({
